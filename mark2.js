71e06363572a2363cbd8438b4306ad2fd7357e13
Joey Gouly <cardologist@gmail.com>
2011-12-28 19:57:09 +0000

    Be a little clever

--- mark2.js
+++ mark2.js
@@ -7,14 +7,13 @@ vm.runInThisContext(fs.readFileSync("./scramble_333.js"));
 vm.runInThisContext(fs.readFileSync("./scramble_NNN.js"));
 vm.runInThisContext(fs.readFileSync("./scramble_sq1.js"));
 
+available_scramblers = ["222", "333", "444", "555", "666", "777", "sq1"];
+
 exports.scramblers = scramblers;
-exports.available_scramblers= ["222", "333", "444", "555", "666", "777", "sq1"];
+exports.available_scramblers = available_scramblers;
 exports.initialize = function() {
-	scramblers["222"].initialize(null, Math);
-	scramblers["333"].initialize(null, Math);
-	scramblers["444"].initialize(null, Math);
-	scramblers["555"].initialize(null, Math);
-	scramblers["666"].initialize(null, Math);
-	scramblers["777"].initialize(null, Math);
-	scramblers["sq1"].initialize(null, Math);
+	for(var i = 0; i < available_scramblers.length; i++)
+	{
+		scramblers[available_scramblers[i]].initialize(null, Math);
+	}
 };
